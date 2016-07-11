2932f76c471b22aad55f87f1fe62543e0435f00d
Ferry Herwanto <nullnet.sample@gmail.com>
2016-07-11 10:01:36 +0700

    Create js get parameters.js

---
+++ js get parameters.js
@@ -0,0 +1,12 @@
+<script>
+function getParameter(url, name) {
+        var r = new RegExp('(\\?|#|&)' + name + '=([^&#\\?]*)(&|#|$|\\?)', 'i');
+        var m = url.match(r);
+        if ((!m || m === ''))
+            m = top.location.href.match(r);
+        return (!m ? '' : m[2]);
+}
+var url = http://google.com/?foo=bar
+alert(getParameter(url, 'foo')); // output bar
+//if you want to use current url, use var url = ''+window.location;
+</script>
