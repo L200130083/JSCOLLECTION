96536245a522d7dee769a84597697be3ef80d2bc
Ferry Herwanto <nullnet.sample@gmail.com>
2016-08-20 23:33:39 +0700

    Update js get parameters.js

--- js get parameters.js
+++ js get parameters.js
@@ -1,4 +1,4 @@
-<script>
+
 function getParameter(url, name) {
         var r = new RegExp('(\\?|#|&)' + name + '=([^&#\\?]*)(&|#|$|\\?)', 'i');
         var m = url.match(r);
@@ -9,4 +9,4 @@ function getParameter(url, name) {
 var url = http://google.com/?foo=bar
 alert(getParameter(url, 'foo')); // output bar
 //if you want to use current url, use var url = ''+window.location;
-</script>
+
